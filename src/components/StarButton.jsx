6e59aee6b30d4165d772f7451f2dbeec4c0dc37a
Simón A Azócar <saimn.azocar@gmail.com>
2023-02-10 17:06:49 -0400

    Add StarButton as react component

---
+++ src/components/StarButton.jsx
@@ -0,0 +1,9 @@
+import { Button } from "antd";
+import { StarFilled, StarOutlined } from "@ant-design/icons";
+
+const StarButton = ({ isFavorite, onClick }) => {
+  const icon = isFavorite ? <StarFilled /> : <StarOutlined />;
+  return <Button icon={icon} onClick={onClick} />;
+};
+
+export { StarButton };
