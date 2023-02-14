2bb2d9b9ff31eee31664a47da551f9e3d13320fe
Simón A Azócar <saimn.azocar@gmail.com>
2023-02-13 21:21:26 -0400

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,45 @@
-import React, { useEffect } from "react";
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import { Col, Spin } from "antd";
-import { Searcher } from "./components/Searcher";
-import { PokemonList } from "./components/PokemonList";
-import { fetchPokemonsWithDetails } from './slices/dataSlice';
-import logo from "./statics/logo.svg";
-import "./App.css";
+import React, { useEffect } from 'react'
+import { shallowEqual, useDispatch, useSelector } from 'react-redux'
+import { Col, Spin } from 'antd'
+import { Searcher } from './components/Searcher'
+import { PokemonList } from './components/PokemonList'
+import { fetchPokemonsWithDetails } from './slices/dataSlice'
+import logo from './statics/logo.svg'
+import './App.css'
 
 const App = () => {
-  const pokemons = useSelector((state) => state.data.pokemons, shallowEqual);
-  const filteredPokemons = useSelector((state) => state.data.searchedPokemons, shallowEqual);
-  const loading = useSelector((state) => state.ui.loading);
-  const dispatch = useDispatch();
+  const pokemons = useSelector((state) => state.data.pokemons, shallowEqual)
+  const filteredPokemons = useSelector(
+    (state) => state.data.searchedPokemons,
+    shallowEqual
+  )
+  const searchValue = useSelector((state) => state.data.searchValue)
+  const loading = useSelector((state) => state.ui.loading)
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(fetchPokemonsWithDetails());
-  }, []);
+    dispatch(fetchPokemonsWithDetails())
+  }, [])
 
   return (
-    <div className="App">
+    <div className='App'>
       <Col span={4} offset={10}>
-        <img className="PokeLogo" src={logo} alt="" />
+        <img className='PokeLogo' src={logo} alt='' />
       </Col>
       <Col span={8} offset={8}>
-        <Searcher pokemons={pokemons}/>
+        <Searcher pokemons={pokemons} />
       </Col>
 
-      {/* {loading ? (
-        <Spin spinning tip="Loading..." size="large" />
+      {loading ? (
+        <Spin spinning tip='Loading...' size='large' />
+      ) : !loading && searchValue ? (
+        <PokemonList pokemons={filteredPokemons} />
       ) : (
         <PokemonList pokemons={pokemons} />
-      )} */}
-
-    <>
-    {filteredPokemons.map((pokemon) => <h1>{pokemon.name}</h1>)}</>
-
+      )}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
 
