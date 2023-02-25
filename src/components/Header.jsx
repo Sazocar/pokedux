261eb1a7430b86b27e181e0ff1dabfa007f2fc7b
Simón A Azócar <saimn.azocar@gmail.com>
2023-02-25 18:46:00 -0400

    FIX width in grid

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import { Col } from 'antd'
 import { Searcher } from './Searcher'
 import { NavButton } from './NavButton'
 import { useSelector } from 'react-redux'
-import { NavLink, useNavigate } from 'react-router-dom'
 import logo from '../statics/logo.svg'
 import './Searcher.css'
 
@@ -12,34 +11,29 @@ const Header = () => {
     (state) => state.data.allPokemonsCounter
   )
   const favoriteCounter = useSelector((state) => state.data.favoriteCounter)
-  const navigate = useNavigate()
-
-  const handleFavorites = () => {
-    navigate('/favorites')
-  }
-
-  const handleHome = () => {
-    navigate('/')
-  }
 
   return (
     <>
-      <Col span={4} offset={10}>
+      <Col span={24} type='flex' align='middle'>
         <img className='PokeLogo' src={logo} alt='' />
       </Col>
-      <Col span={8} offset={8}>
+      <Col span={24} type='flex' align='middle'>
         <Searcher />
       </Col>
 
       <Col span={24} type='flex' align='middle'>
-
-        <NavButton text={'All'} count={allPokemonsCounter} to={'/'}/>
-        <NavButton text={'Favorites'} count={favoriteCounter} to={'/favorites'}/>
-
+        <NavButton text={'All'} count={allPokemonsCounter} to={'/'} />
+        <NavButton
+          text={'Favorites'}
+          count={favoriteCounter}
+          to={'/favorites'}
+        />
       </Col>
     </>
   )
 }
 
 export { Header }
+
+
 
