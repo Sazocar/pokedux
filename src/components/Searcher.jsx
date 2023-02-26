e0577a790b0d0c402bdc21709700db620f32f79e
Simón A Azócar <saimn.azocar@gmail.com>
2023-02-25 20:05:15 -0400

    ADD loader in search component

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,36 +1,43 @@
-import { useDispatch } from "react-redux"
-import { setSearchedFavoritePokemons, setSearchedPokemons, setSearchValue } from "../slices/dataSlice"
-import { useLocation } from "react-router-dom"
-import { Input } from "antd"
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  setSearchedFavoritePokemons,
+  setSearchedPokemons,
+  setSearchValue,
+} from '../slices/dataSlice'
+import { useLocation } from 'react-router-dom'
+import { Input } from 'antd'
 import './Searcher.css'
 
-
 const Searcher = () => {
-  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.ui.loading)
+  const dispatch = useDispatch()
   const { pathname } = useLocation()
-  
+
   const onChangeValue = (e) => {
     dispatch(setSearchValue({ value: e.target.value.toLowerCase() }))
-    
+
     if (pathname === '/favorites') {
-      dispatch(setSearchedFavoritePokemons({ value: e.target.value.toLowerCase() }))
+      dispatch(
+        setSearchedFavoritePokemons({ value: e.target.value.toLowerCase() })
+      )
     } else {
       dispatch(setSearchedPokemons({ value: e.target.value.toLowerCase() }))
     }
   }
 
   return (
-    <Input.Search onChange={onChangeValue} className='Searcher' placeholder='Search...'/>
+    <Input.Search
+      className='Searcher'
+      placeholder='Search pokemons...'
+      size='large'
+      allowClear
+      disabled={loading}
+      enterButton='Search'
+      onChange={onChangeValue}
+    />
   )
 }
 
+export { Searcher }
 
 
-
-
-
-
-
-
-
-export { Searcher };
